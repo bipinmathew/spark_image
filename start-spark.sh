21781c63cbdca5e233e6987a25a854265fe206a8
Bipin Mathew <bipinmathew@gmail.com>
2017-12-10 17:58:21 -0500

    Fixing a bad commit.

--- start-spark.sh
+++ start-spark.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #Create a spark user and group from the passed in arguments.
-#Arguments [master|worker] uid gid
-groupadd -g $3 spark
-useradd -ms /bin/bash -u $2 -g $3 spark && chown spark:spark /home/spark && chown -R spark:spark $SPARK_HOME
+#args uid gid [master|worker] host port [webport]
+groupadd -g $2 spark
+useradd -ms /bin/bash -u $1 -g $2 spark && chown spark:spark /home/spark && chown -R spark:spark $SPARK_HOME
 echo "export SPARK_HOME=$SPARK_HOME" >> /home/spark/.profile
 echo "export PYTHONPATH=\$SPARK_HOME/python:\$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH" >> /home/spark/.profile
 echo "export PATH=\$SPARK_HOME/bin:\$PATH" >> /home/spark/.profile
-su - spark
-
+if [ $3 = "master"]
+then
+  su - spark -c "start-master $4 $5 $6" 
+elif[ $3 = "worker"]
+  su - spark -c "start-worker $4 $5" 
+then
+  echo "No start mode for spark specified."
+  exit 1
+fi
 #su - jupyter -c "jupyter notebook --ip=0.0.0.0 --port=8080 &> /dev/null"
-
